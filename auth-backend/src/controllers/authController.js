3807f48335d12c4399587520d71a470591caf1c4
shivanirathore24 <shivani.rathore2409@gmail.com>
2026-03-20 21:56:21 +0530

    feat: password hashing

--- auth-backend/src/controllers/authController.js
+++ auth-backend/src/controllers/authController.js
@@ -1,31 +1,20 @@
-import bcrypt from "bcrypt";
 import User from "../models/User.js";
 
 export const signup = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    const isValidPassword = /^(?=.*[a-zA-Z])(?=.*[0-9])[A-Za-z0-9]{6,}$/.test(
-      password,
-    );
-
-    if (!isValidPassword) {
-      return res.status(400).json({
-        message:
-          "Password must be at least 6 characters and contain letters and numbers",
-      });
-    }
-
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+    // No hashing here anymore
+    // model will handle it
 
     const user = new User({
       email,
-      password: hashedPassword,
+      password,
     });
 
     await user.save();
@@ -34,6 +23,12 @@ export const signup = async (req, res) => {
       message: "User registered successfully",
     });
   } catch (error) {
+    if (error.name === "ValidationError") {
+      return res.status(400).json({
+        message: error.message,
+      });
+    }
+
     console.error(error);
     res.status(500).json({
       message: "Server error",
--- auth-backend/src/models/User.js
+++ auth-backend/src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
   {
@@ -10,17 +11,30 @@ const userSchema = new mongoose.Schema(
       trim: true,
       match: [/^\S+@\S+\.\S+$/, "Please use a valid email address"],
     },
-
     password: {
       type: String,
       required: true,
       minlength: 6,
+      validate: {
+        validator: function (value) {
+          // allows letters + numbers + special characters
+          return /^(?=.*[a-zA-Z])(?=.*[0-9]).{6,}$/.test(value);
+        },
+        message:
+          "Password must be at least 6 characters and contain both letters and numbers",
+      },
     },
   },
   {
     timestamps: true,
   },
 );
+
+// PRE-SAVE HOOK (HASH PASSWORD)
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
 
 const User = mongoose.model("User", userSchema);
 
